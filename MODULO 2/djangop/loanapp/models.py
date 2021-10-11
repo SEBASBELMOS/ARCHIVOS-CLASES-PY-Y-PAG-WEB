92d4ad446dd1244988bd5a01dfcabde687cfdebd
Sebastian Belalcazar <89162606+SEBASBELMOS@users.noreply.github.com>
2021-10-11 14:43:36 -0500

    update models

--- MODULO 2/djangop/loanapp/models.py
+++ MODULO 2/djangop/loanapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class cita(models.Model):
+    servicio = models.CharField(max_length=30)
+    precio = models.FloatField()
+    duracion = models.DurationField()
+
+    def __str__(self):
+        return self.titulo
