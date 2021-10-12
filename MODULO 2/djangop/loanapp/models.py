2a6cc39df7610d9d6f4b136c273e81949924a955
Sebastian Belalcazar <89162606+SEBASBELMOS@users.noreply.github.com>
2021-10-12 14:56:11 -0500

    update pag web with django

--- MODULO 2/djangop/loanapp/models.py
+++ MODULO 2/djangop/loanapp/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields.related import OneToOneField
 
 # Create your models here.
 
+serv_opc = [
+    ('M','Maquillaje'),
+    ('TC','Tratamientos Corporales'),
+    ('TF','Tratamientos Faciales'),
+    ('E','Especial')
+]
+
+class serv(models.Model):
+    fecha = models.DateField(max_length=13)
+    servicio = models.CharField(max_length=50, choices=serv_opc)
+
 class cita(models.Model):
-    servicio = models.CharField(max_length=30)
     precio = models.FloatField()
     duracion = models.DurationField()
 
-    def __str__(self):
-        return self.servicio
+class Persona(models.Model):
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(max_length=40)
+
